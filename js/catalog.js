c484655205c5a65ee5393b4fa674a85f9b2b17ae
tihonchik <tihonchik1@gmail.com>
2026-04-27 00:54:41 +0300

    feat: conect json server

--- js/catalog.js
+++ js/catalog.js
@@ -1,18 +1,9 @@
 import Good from "/models/Good.js";
 import CatalogCard from "/compоnents/CatalogCard.js";
 
-const goods = [];
+const baseUrl = "http://localhost:3000/";
 
-const newGood = new Good(
-  "Blush Compact - Peach",
-  "A natural-looking blush for a healthy, rosy glow.",
-  14.99,
-  "https://daplast.ru/local/cache/image/28/74/2483/banka-pet-kameliya-250-matovaya.jpg?v=1774619365",
-  "Soft Cheeks",
-  "5 g",
-);
-
-let displayedGoods = structuredClone(goods);
+let displayedGoods = [];
 
 const grid = document.querySelector(".catalog-grid");
 
@@ -35,72 +26,13 @@ function renderCatalog(list) {
   });
 }
 
-renderCatalog(goods);
+try {
+  const response = await fetch(baseUrl + "goods");
+  const result = await response.json();
+  displayedGoods = result;
+} catch {}
 
-const reset_button = document.querySelector(".reset-button");
-reset_button.addEventListener("click", (e) => {
-  renderCatalog(goods);
-});
-
-const first_button = document.querySelector(".first-button");
-first_button.addEventListener("click", (e) => {
-  const newGoods = displayedGoods.map((x) => x.addToCoast(x.coast));
-  renderCatalog(newGoods);
-});
-
-const second_button = document.querySelector(".second-button");
-second_button.addEventListener("click", (e) => {
-  const newGoods = displayedGoods.filter((x) => x.coast > 1000);
-  renderCatalog(newGoods);
-});
-
-const third_button = document.querySelector(".third-button");
-third_button.addEventListener("click", (e) => {
-  const newGoods = displayedGoods.sort((a, b) => a.coast - b.coast);
-  renderCatalog(newGoods);
-});
-
-const fourth_button = document.querySelector(".fourth-button");
-fourth_button.addEventListener("click", (e) => {
-  const newGoods = displayedGoods.sort((a, b) => b.coast - a.coast);
-  renderCatalog(newGoods);
-});
-
-const fiveth_button = document.querySelector(".fiveth-button");
-fiveth_button.addEventListener("click", () => {
-  const newGoods = displayedGoods.slice(0, -1);
-  renderCatalog(newGoods);
-});
-
-const sixth_button = document.querySelector(".sixth-button");
-sixth_button.addEventListener("click", () => {
-  displayedGoods.push(newGood);
-  renderCatalog(displayedGoods);
-});
-
-const seventh_button = document.querySelector(".seventh-button");
-seventh_button.addEventListener("click", () => {
-  const newGoods = displayedGoods.concat(displayedGoods);
-  renderCatalog(newGoods);
-});
-
-const eight_button = document.querySelector(".eight-button");
-eight_button.addEventListener("click", () => {
-  const newGoods = [displayedGoods.find((x) => x.title.includes("Cream"))];
-  renderCatalog(newGoods);
-});
-
-const nineth_button = document.querySelector(".nineth-button");
-nineth_button.addEventListener("click", () => {
-  const newGoods = displayedGoods.reverse();
-  renderCatalog(newGoods);
-});
-
-const tenth_button = document.querySelector(".tenth-button");
-tenth_button.addEventListener("click", () => {
-  const newGoods = [...displayedGoods].sort(() => Math.random() - 0.5);
-  renderCatalog(newGoods);
-});
+renderCatalog(displayedGoods);
 
 const sort = document.querySelector(".catalog_sort");
 sort.addEventListener("change", () => {
